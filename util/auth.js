c83d0c3bc0b1f81f9c7cdbeb9b0ca4e8aa75a70e
eung-eung <boyvip5231@gmail.com>
2024-03-07 15:17:11 +0700

    handle send otp and  signUp auth function

--- util/auth.js
+++ util/auth.js
@@ -1,16 +1,30 @@
+
 import axios from '../lib/axios'
 
 export async function getUser(email, password) {
-    console.log('vô getuser');
-    console.log('email: ', email);
-    console.log('password: ', password);
-
     const response = await axios.post('/Auth/login', {
         email: email,
         password: password
     })
-    console.log('response: ', response.data);
     const token = response.data.accessToken
     return token
 
+}
+
+export async function sendOtpToUser({ email, password, firstName, lastName }) {
+    if (email && password && firstName && lastName) {
+        const response = await axios.post('/User/send-OTP-email', email)
+        return response.status
+    }
+}
+
+export async function signUpUser(email, password, firstName, lastName, otp) {
+    const response = await axios.post('/User/register-student', {
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        otpEmailCode: otp
+    })
+    return response
 }

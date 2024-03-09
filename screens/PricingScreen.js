da3da143f73afb09f8ac3b1ef5a7f0b240ae775e
eung-eung <boyvip5231@gmail.com>
2024-03-09 21:32:19 +0700

    handle webview vnpay

--- screens/PricingScreen.js
+++ screens/PricingScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   StyleSheet,
   View,
@@ -6,11 +6,14 @@ import {
   TouchableOpacity,
   Image,
   ScrollView,
-  Linking
+  Linking,
+  AppState
 } from 'react-native';
 import { axiosAuth } from '../lib/axios';
 import useAxiosAuth from '../lib/hooks/useAxiosAuth';
-
+import WebView from 'react-native-webview';
+import LoadingOverlay from '../components/UI/LoadingOverlay';
+import { useNavigation } from '@react-navigation/native';
 
 const packageItem = [
   { id: 1, name: 'Fistful of Gems', price: 25000, value: 200, image: "https://i.pinimg.com/564x/a4/d4/57/a4d457d3dff578f6468e1f19ed7b5afd.jpg" },
@@ -22,48 +25,129 @@ const packageItem = [
 ];
 
 export default function Pricing({ navigation }) {
+  const getLink = async () => {
+    const link = await Linking.getInitialURL()
+    console.log('link: ', link);
+  }
+
+
   const [selectedItem, setSelectedItem] = useState(null);
-  const [deeplink, setDeeplink] = useState(null)
+  const appState = useRef(AppState.currentState);
+  const [appStateVisible, setAppStateVisible] = useState(appState.current);
+  const [sourceUri, setSourceUri] = useState()
   const axiosAuth = useAxiosAuth()
+  const [isLoading, setIsLoading] = useState(false)
+  const [show, setShow] = useState(false)
+  const nav = useNavigation()
   const handlePayment = async (value) => {
+    setIsLoading(true)
     const response = await axiosAuth.post('/TransactionWallet/deposit/vnpay', {
       'amount': value,
-      'redirectUrl': 'exp://192.168.1.4:8081/MyAccount'
-
+      'redirectUrl': 'https://mind-master-minds-payment-result.vercel.app/'
     })
-    const link = await Linking.openURL(response.data.paymentUrl)
+    setIsLoading(false)
+    setSourceUri(response.data.paymentUrl)
+    setShow(true)
 
   };
+  console.log('uri: ', sourceUri);
+  const injectedJavascript = `(function() {
+    window.postMessage = function(data) {
+  window.ReactNativeWebView.postMessage(data);
+};
+})()`
+  //   const injectedJavascript = `(function() {
+  //     window.postMessage = function(data) {
+  //   window.ReactNativeWebView.postMessage(data);
+  // };
+  // })()`
 
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      if (
+        appState.current.match(/inactive|background/) &&
+        nextAppState === 'active'
+      ) {
+        console.log('App has come to the foreground!');
+        getLink()
+
+      }
+
+      appState.current = nextAppState;
+      setAppStateVisible(appState.current);
+      console.log('AppState', appState.current);
+      getLink()
+    });
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
   return (
-    <ScrollView style={{ flex: 1, backgroundColor: '#fafafa' }}>
-      <View style={styles.container}>
-        <Text style={styles.title}>Pick your plan</Text>
+    <>
+      {isLoading && <LoadingOverlay />}
+      {show &&
+        <WebView
+          injectedJavaScript={injectedJavascript}
+          startInLoadingState
+          scalesPageToFit
+          style={{ flex: 1 }}
+          source={{ uri: sourceUri }}
+          javaScriptEnabled
+          onMessage={(e) => {
+            nav.replace('BottomTabs')
+          }}
+          onNavigationStateChange={(navState => {
+            console.log('webview navstate: ', navState.vnp_ResponseCode);
+            console.log('native url: ', navState.url);
+            if (navState.url.includes('vnp_ResponseCode')) {
+              // Extract the response code or other relevant information from the URL
+              const urlParams = new URLSearchParams(navState.url);
+              const responseCode = urlParams.get('vnp_ResponseCode');
+              const transactionNo = urlParams.get('vnp_TransactionNo');
+              const paymentStatus = responseCode === '00' ? 'Successful' : 'Failed';
 
-        {packageItem.map((item) => (
-          <TouchableOpacity
-            key={item.id}
-            onPress={async () => {
-              setSelectedItem(item);
-              handlePayment(item.price)
-            }}
-          >
-            <View style={[styles.radio, selectedItem === item && styles.radioActive]}>
-              <View style={styles.card}>
-                <View style={styles.content}>
-                  <Text style={styles.radioLabel}>{item.name}</Text>
-                  <Text style={styles.radioPrice}>{item.price.toLocaleString()} VND</Text>
+              // Handle the payment status accordingly
+              console.log(`Payment Status: ${paymentStatus}, Transaction No: ${transactionNo}`);
+
+              // Close the WebView or navigate as needed
+            }
+          })}
+        />
+      }
+      {!show &&
+        <ScrollView style={{ flex: 1, backgroundColor: '#fafafa' }}>
+
+          <View style={styles.container}>
+            <Text style={styles.title}>Pick your plan</Text>
+
+            {packageItem.map((item) => (
+              <TouchableOpacity
+                key={item.id}
+                onPress={async () => {
+                  setSelectedItem(item);
+                  handlePayment(item.price)
+                }}
+              >
+                <View style={[styles.radio, selectedItem === item && styles.radioActive]}>
+                  <View style={styles.card}>
+                    <View style={styles.content}>
+                      <Text style={styles.radioLabel}>{item.name}</Text>
+                      <Text style={styles.radioPrice}>{item.price.toLocaleString()} VND</Text>
+                    </View>
+                    <Image
+                      source={{ uri: item.image }}
+                      style={styles.radioImage}
+                    />
+                  </View>
                 </View>
-                <Image
-                  source={{ uri: item.image }}
-                  style={styles.radioImage}
-                />
-              </View>
-            </View>
-          </TouchableOpacity>
-        ))}
-      </View>
-    </ScrollView>
+              </TouchableOpacity>
+            ))}
+          </View>
+        </ScrollView>
+      }
+
+    </>
   );
 }
 
